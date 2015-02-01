47594360f205ef106fc3b56b5f6d8f4de5cc4af0
DiCookieMonster <dina.suranovich@gmail.com>
2015-02-02 00:05:08 +0800

    Create QueueIsEmpty.java

---
+++ QueueIsEmpty.java
@@ -0,0 +1,14 @@
+/**
+ * @author DinaSu
+ */
+
+public class QueueIsEmpty extends Exception {
+private static final long serialVersionUID = 2922799982337908495L; //some generated serial
+
+/*
+* Exception message
+*/
+public QueueIsEmpty(){
+    System.out.println("Queue Is Empty.\n");
+    }
+}
